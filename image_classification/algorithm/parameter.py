0a6830ccd5616b04b06f316f6efc1f0265c6954c
gpx <1457495424@qq.com>
2020-04-28 17:39:01 +0800

    所有功能已实现

--- image_classification/algorithm/parameter.py
+++ image_classification/algorithm/parameter.py
@@ -5,7 +5,6 @@ num_task = 4
 num_class = 10
 
 path = settings.PARA_PATH
-
 input_hidden_weights = np.load(path + 'input_hidden_weights.npy')
 hidden_output_weight = np.load(path + 'hidden_output_weight.npy')
 task_embedding_vectors = np.load(path + 'task_embedding_vectors.npy')
